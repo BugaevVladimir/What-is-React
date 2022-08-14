4f92d52762e7469bd9bd67ec538cef86994e9d91
BugaevVladimir <101861875+BugaevVladimir@users.noreply.github.com>
2022-08-14 17:52:26 +0300

    14.08.22
    Формы. Вывод в список

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ### Что уже знаю из React
 
+*14.08.22*
 - Первые компоненты из чего строится структура работы в React и свойство **Props**
 - Состояние компонента **State**
+- Формы. Вывод в список
 
 
 ###### Изучаю React по [видеокурсу](https://www.youtube.com/playlist?list=PLVfMKQXDAhGV90yI1FzsciwEi-11ILfAG)
--- what-is-react/src/Ccomponent.js
+++ what-is-react/src/Ccomponent.js
@@ -5,39 +5,43 @@ export default class Ccomponent extends Component {
     super(props)
   
     this.state = {
-      count: 0
+      input: "",
+      items: []
     };
 
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
-  increment() {
-    this.setState(state => ({
-      count: state.count + 1
-    }));
-  }
-  decrement() {
-    this.setState(state => ({
-      count: state.count - 1
-    }));
-  }
-  reset() {
+  handleChange(event) {
     this.setState({
-      count: 0
+      input: event.target.value
     })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      input: this.state.input,
+      items: [...this.state.items, this.state.input]
+    });
   }
 
 
   render() {
     return(
       <div>
-        <button onClick={this.increment}>Добавить + 1</button>
-        <button onClick={this.decrement}>Убрать - 1</button>
-        <button onClick={this.reset}>Reset</button>
-        <h1>Current: {this.state.count}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.input} onChange={this.handleChange} />
+          <button type="submit">Показать</button>
+        </form>
+
+        <ul>
+          {this.state.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
